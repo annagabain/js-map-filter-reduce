996b8b3af631c63d7cd4fe3b7eebcc6ec1443e96
Anna Gabain <anna.gabain@outlook.com>
2022-10-30 17:03:42 +0000

    Filter method 2 lesson

--- filter.js
+++ filter.js
@@ -60,3 +60,10 @@ const people = [
       ]
     },
   ];
+
+  const has5yearsExp = skill => skill.yrsExperience >= 5
+  const hasStrongSkills = student=> student.skills.filter(has5yearsExp).length > 0;
+  const candidates = students.filter(hasStrongSkills)
+console.log(candidates)
+
+  
